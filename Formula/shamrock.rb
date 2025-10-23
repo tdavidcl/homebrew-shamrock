028793e2901c7c9a6a1c57c5351dce59c374f6d7
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-10-23 13:38:39 +0200

    .

--- Formula/shamrock.rb
+++ Formula/shamrock.rb
@@ -38,7 +38,7 @@ class Shamrock < Formula
       -DSYCL_IMPLEMENTATION=ACPPDirect
       -DCMAKE_CXX_COMPILER=acpp
       -DACPP_PATH=#{Formula["adaptivecpp"].opt_prefix}
-      -DCMAKE_INSTALL_PYTHONDIR=#{prefix/Language::Python.site_packages(python3)}
+      -DCMAKE_INSTALL_PYTHONDIR=#{prefix/Language::Python.site_packages(python)}
       -DSHAMROCK_EXTERNAL_FMTLIB=ON
       -DSHAMROCK_EXTERNAL_JSON=ON
       -DSHAMROCK_EXTERNAL_PYBIND11=ON
