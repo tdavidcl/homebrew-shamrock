cffaad7c28c3b25d301703c4ea6f686419b09c71
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-10-23 13:35:12 +0200

    .

--- Formula/shamrock.rb
+++ Formula/shamrock.rb
@@ -2,7 +2,7 @@ class Shamrock < Formula
   desc "Astrophysical hydrodynamics using SYCL"
   homepage "https://github.com/Shamrock-code/Shamrock"
   url "file:///Users/davidclt/Downloads/Shamrock.tar", using: :nounzip
-  sha256 "59d5652467fd9453a65ae7b48e0c9b7d4162edc8df92e09d08dcc5275407a897"
+  sha256 "0ca54e35f88f4b7e14c372933d0cea96b11fa43c0b9248291b823eb5989fa1f2"
   version "2025.05.0"
   license "CECILL-2.1"
   revision 1
